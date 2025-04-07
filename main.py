cbc7cb37867d1361109b08f840490583071bf230
Jana <yanix2x2@gmail.com>
2025-04-07 19:13:24 +0300

    add template.html and main.py

--- main.py
+++ main.py
@@ -1,5 +1,59 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import datetime
+from collections import defaultdict
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import pandas
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def year_format(age):
+    if 11 <= age % 100 <= 19:
+        return 'лет'
+
+    num = age % 10
+    if num == 1:
+        return 'год'
+    elif 2 <= num <= 4:
+        return 'года'
+    
+    return 'лет'
+
+
+def get_product_categories():
+    excel_data_product = pandas.read_excel(
+        'wine.xlsx',
+        na_values='None',
+        keep_default_na=False
+    ).to_dict(orient='records')
+
+    products = defaultdict(list)
+    for product in excel_data_product:
+        products[product['Категория']].append(product)
+    return dict(products)
+
+
+def main():
+    age = datetime.datetime.now().year - 1920
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        year=age,
+        year_format=year_format(age),
+        categories=get_product_categories()
+
+    )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
